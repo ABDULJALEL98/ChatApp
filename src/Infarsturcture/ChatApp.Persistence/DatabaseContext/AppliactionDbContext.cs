899b93d368c136d733656c6aa5924807308b01b5
ABDUL JALEL ALISMAAEL <97349470+ABDULJALEL98@users.noreply.github.com>
2024-03-16 14:59:09 +0300

    DatabaseContext

---
+++ src/Infarsturcture/ChatApp.Persistence/DatabaseContext/AppliactionDbContext.cs
@@ -0,0 +1,30 @@
+﻿using ChatApp.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Persistence.DatabaseContext;
+
+ public class AppliactionDbContext : DbContext
+{
+    public AppliactionDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppliactionDbContext).Assembly);
+    }
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+        {
+            if(entry.State == EntityState.Modified)
+                entry.Entity.ModifiedDate = DateOnly.FromDateTime(DateTime.UtcNow);
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+    public DbSet <ChatApp.Domain.Entities.Message> Messages { get; set; }
+}
