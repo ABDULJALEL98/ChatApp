ee58899ce55e1a88c2c79ce568325e9eef456840
ABDUL JALEL ALISMAAEL <97349470+ABDULJALEL98@users.noreply.github.com>
2024-03-18 15:45:18 +0300

    first Controller

---
+++ src/Presentation/API/ChatApp.API/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public BaseController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+}
---
+++ src/Presentation/API/ChatApp.API/Controllers/MessagesController.cs
@@ -0,0 +1,24 @@
+﻿using ChatApp.Application.Features.Message.Query.GetAllMessages;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MessagesController : BaseController
+{
+    private readonly IMediator _mediator;
+
+    public MessagesController(IMediator mediator) : base(mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpGet]
+    public async Task<ActionResult<MessageReturnDto>> Get()
+    {
+        var messages = await _mediator.Send(request: new GetAllMessageQuery());
+            return Ok(messages);
+    }
+}
