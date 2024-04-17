fce35edf2e555995f1328ba83ad9aa6386e5e341
ABDUL JALEL ALISMAAEL <97349470+ABDULJALEL98@users.noreply.github.com>
2024-04-17 16:43:47 +0300

    Setting up Identity packages

---
+++ src/Core/ChatApp.Domain/Entities/Photo.cs
@@ -0,0 +1,24 @@
+﻿using ChatApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Domain.Entities;
+
+public class Photo:BaseEntity
+{
+    public DateTime DateOfBirth { get; set; }
+    public string KnownAs { get; set; } = string.Empty;
+    public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime LastActive { get; set; } = DateTime.Now;
+    public string Gender { get; set; } = string.Empty;
+    public string Introduction { get; set; } = string.Empty;
+    public string LookingFor { get; set; } = string.Empty;
+    public string Interests { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public ICollection<Photo> Photos { get; set; }
+
+}
